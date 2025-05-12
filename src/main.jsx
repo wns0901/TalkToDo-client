451e528b2ff0bb0d75bc80b658ae64c240756a35
wns0901 <wnswns012@gmail.com>
2025-05-12 23:14:47 +0900

    Feat(): 기본라우팅 설정

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { createRoot } from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { LoginContextProvider } from "./context/LoginContext.jsx";
+import MainPage from "./domains/MainPage/MainPage.jsx";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+createRoot(document.getElementById("root")).render(
+  <BrowserRouter>
+    <LoginContextProvider>
+      <Routes>
+        <Route path="/" element={<MainPage />} />
+      </Routes>
+    </LoginContextProvider>
+  </BrowserRouter>
+);
